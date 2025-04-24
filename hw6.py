87e075a9bb4b374b396063f6322abbcde470dbcc
leehankyul <caleb00824@gmail.com>
2025-04-24 23:16:50 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,11 @@
+for i in range(1, 10):
+    for d in range(2, 6):
+        print(f"{d} x {i} = {d * i:2}", end='\t')
+    print()
+
+print()
+
+for i in range(1, 10):
+    for d in range(6, 10):
+        print(f"{d} x {i} = {d * i:2}", end='\t')
+    print()
